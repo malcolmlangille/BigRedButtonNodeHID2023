db640c84041cf89c63aea64094db22d94bad184a
Malcolm Langille <malcolmlangille@gmail.com>
2023-09-04 21:22:43 -0400

    Updated code to use json properties file for strings and executables. Button pressed will open chrome

--- BigRedButtonTest.js
+++ BigRedButtonTest.js
@@ -1,21 +1,48 @@
 // MIT licensed. (C) Dj Walker-Morgan 2018
-
 var BigRedButton = require('./BigRedButton');
+const fs = require('fs');
+const path = require('path');
+
+// Get the path to the event_actions.json file from command line arguments
+const eventActionsPath = process.argv[2] || './event_actions.json';
+
+// Set the environment variable for the event actions file path
+process.env.EVENT_ACTIONS_PATH = eventActionsPath;
+
+let eventActions = {};
+try {
+    const rawData = fs.readFileSync(path.resolve(__dirname, eventActionsPath));
+    eventActions = JSON.parse(rawData);
+} catch (error) {
+    console.error('Failed to load event actions:', error);
+}
 
 function configureButton(button) {
   button.on('buttonPressed', function () {
-    console.log('button pressed');
+    console.log(eventActions.buttonPressed ? eventActions.buttonPressed.message : 'Button pressed');
+    if (eventActions.buttonPressed && eventActions.buttonPressed.executable) {
+        require('child_process').exec(eventActions.buttonPressed.executable);
+    }
   });
 
   button.on('buttonReleased', function () {
-    console.log('button released');
+    console.log(eventActions.buttonReleased ? eventActions.buttonReleased.message : 'Button released');
+    if (eventActions.buttonReleased && eventActions.buttonReleased.executable) {
+        require('child_process').exec(eventActions.buttonReleased.executable);
+    }
   });
 
   button.on('lidRaised', function () {
-    console.log('lid raised');
+    console.log(eventActions.lidRaised ? eventActions.lidRaised.message : 'Lid raised');
+    if (eventActions.lidRaised && eventActions.lidRaised.executable) {
+        require('child_process').exec(eventActions.lidRaised.executable);
+    }
   });
   button.on('lidClosed', function () {
-    console.log('lid closed');
+    console.log(eventActions.lidClosed ? eventActions.lidClosed.message : 'Lid closed');
+    if (eventActions.lidClosed && eventActions.lidClosed.executable) {
+        require('child_process').exec(eventActions.lidClosed.executable);
+    }
   });
 
   button.on('buttonGone', function () {
